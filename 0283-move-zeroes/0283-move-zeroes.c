bd20b7cfc6e9f99258e92837b87c738464f1bf72
Snyong <62795814+spencer0124@users.noreply.github.com>
2024-10-17 01:15:33 +0900

    Time: 72 ms (82.96%), Space: 19.5 MB (46.79%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,13 +1,17 @@
 void moveZeroes(int* nums, int numsSize) {
     int zeroCount = 0;
+    int idx = 0;
 
-    for(int i=numsSize-1; i>=0; i--) {
-        if(nums[i] ==0) {
-            for(int j=i; j<numsSize-1; j++) {
-                int temp = nums[j];
-                nums[j]=nums[j+1];
-                nums[j+1] = temp;
-            }
-        }
+    for(int i=0; i<numsSize; i++) {
+        // 값이 0이 아닌 경우에만 추가
+        // idx는 항상 i보다 작거나 같으므로 한 배열 안에서 처리 가능
+        if(nums[i]) nums[idx++] = nums[i];
+        else zeroCount++;
     }
+
+    // zeroCount로 0의 개수를 파악해서, 개수만큼 0을 뒤에 더해주기 
+    for(int i=0; i<zeroCount; i++) {
+        nums[idx++] = 0;
+    }
+
 }
